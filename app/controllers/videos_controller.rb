bf94414437358607a19b9124757346d87e9207c3
Anthony Roberts <roberts.anthony@gmail.com>
2014-06-11 22:26:54 +0000

    fix redirect bug

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -11,7 +11,7 @@ class VideosController < ApplicationController
     @video = Video.new(video_params)
     if @video.save
       flash[:success] = 'Video added!'
-      redirect_to 'videos#index'
+      redirect_to '/videos#index'
     else
       render 'new'
     end
