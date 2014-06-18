e3c67bb024919bc542bf5745b1488928c33cc91d
Anthony Roberts <roberts.anthony@gmail.com>
2014-06-18 22:01:39 +0000

    change home and about pages

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,15 @@
 module UsersHelper
 
 	# Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user, options = { size: 50 })
+  def gravatar_for(user, options = { size: 150 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar img-circle")
   end
 
-  def gravatar_main(options = { size: 50 })
+  def gravatar_main(options = { size: 200 })
   	gravatar_id = Digest::MD5::hexdigest('roberts.anthony@gmail.com')
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=200"
     image_tag(gravatar_url, class: "gravatar img-circle")
   end
 end
